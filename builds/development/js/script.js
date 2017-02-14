ba41f10815b812502eb085ecd4ec74eefa4b4f54
Bryson Gabrels <brysongabrels@gmail.com>
2017-02-13 21:33:05 -0600

    working responsive video, streaming

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -9,22 +9,6 @@ function homeVid() {
 
 setTimeout(homeVid, 5000);
 
-//$( window ).resize(function() {
-//	var width = $(window).width();
-//	var height = $(window).height();
-//	console.log(height);
-//	$('.homeVideo').css("width", width + "px");
-//	$('.homeVideo').css("height", height + "px");
-//
-//	if((width/height) >= ($('.homeVideo').width())/ $('.homeVideo').height()) {
-//		console.log("fuckin a");
-//	} else {
-//		console.log("fuckin b");
-//	}
-//
-//});
-
-
 
 
 var pixgrid = function() {
