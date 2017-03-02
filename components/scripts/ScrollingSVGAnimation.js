0744ae5998a57297e8abbaad7a448ec002c15775
Bryson Gabrels <brysongabrels@gmail.com>
2017-03-01 21:59:57 -0600

    added placeholder images

--- components/scripts/ScrollingSVGAnimation.js
+++ components/scripts/ScrollingSVGAnimation.js
@@ -8,7 +8,7 @@ var logo = $('.logo');
 $(window).scroll(function() {
 	var pos = $(window).scrollTop();
 	var opacity = 1 - (pos / 400);
-	$('.testing').text("p" + opacity);
+	
 	
 
 	logo.css({'opacity': opacity});
@@ -18,5 +18,19 @@ $(window).scroll(function() {
 	} else if (opacity < '0') {
 		logo.css({'opacity':0});
 	}
+
 	
+
+
+	$('.path').each(function() {
+	var imagePos = $(this).offset().top;
+
+	var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow + distanceFromTop) {
+			$(this).addClass("animate");
+		}
+
+		$('.testing').text(imagePos);
+	});
 });
+
